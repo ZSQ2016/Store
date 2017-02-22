515412fd44784a506ec893a79bd29151f37460ff
zhanshunqing <674263615@qq.com>
2017-02-22 16:10:22 +0800

    service

---
+++ store Maven Webapp/src/main/java/com/cn/store/service/IUserService.java
@@ -0,0 +1,8 @@
+package com.cn.store.service;
+
+import com.cn.store.pojo.User;
+
+public interface IUserService
+{
+	public User getUserById(int userId);  
+}
---
+++ store Maven Webapp/src/main/java/com/cn/store/service/impl/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package com.cn.store.service.impl;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.cn.store.dao.IUserDao;
+import com.cn.store.pojo.User;
+import com.cn.store.service.IUserService;
+
+
+@Service("userService")  
+public class UserServiceImpl  implements IUserService {  
+	@Resource  
+	private IUserDao userDao;  
+	@Override  
+	public User getUserById(int userId) {  
+		// TODO Auto-generated method stub  
+		return this.userDao.selectByPrimaryKey(userId); 
+	}
+}
+
